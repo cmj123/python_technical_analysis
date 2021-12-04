3ce2d1699a2e685e583c3e4c3cc79cdba19de72e
Esuabom Dijemeni <e.dijemeni@gmail.com>
2021-12-04 05:34:30 +0000

    Completed method set_parameters and test_strategy for SMABacktester

--- section05-sma/SMABacktester.py
+++ section05-sma/SMABacktester.py
@@ -49,4 +49,46 @@ class SMABacktester():
         self.start = start
         self.end = end
         self.results = None 
-        
+        self.get_data()
+
+
+    def get_data(self):
+        '''
+        Retrieves and prepares the data
+        '''
+        raw = pd.read_csv("forex_pairs.csv", parse_dates=["Date"], index_col="Date")
+        raw = raw[[self.symbol]].dropna()
+        raw = raw.loc[self.start:self.end]
+        raw.rename(columns={self.symbol:"price"}, inplace=True)
+        raw["returns"] = np.log(raw / raw.shift(1))
+        raw["SMA_S"] = raw["price"].rolling(self.SMA_S).mean()
+        raw["SMA_L"] = raw["price"].rolling(self.SMA_L).mean()
+        self.data = raw
+
+    def set_parameters(self, SMA_S=None, SMA_L=None):
+        '''
+        Update SMA parameters and resp. time series
+        '''
+        if SMA_S is not None:
+            self.SMA_S = SMA_S
+            self.data["SMA_S"] = self.data["price"].rolling(self.SMA_S).mean()
+        if SMA_L is not None:
+            self.SMA_L= SMA_L
+            self.data["SMA_L"] = self.data["price"].rolling(self.SMA_L).mean()
+
+    def test_strategy(self):
+        '''Backtests the trading strategy
+        '''
+        data = self.data.copy().dropna()
+        data["position"]=np.where(data["SMA_S"] > data["SMA_L"], 1, -1)
+        data["strategy"] = data["position"].shift(1)* data["returns"]
+        data.dropna(inplace=True)
+        data["creturns"] = data["returns"].cumsum().apply(np.exp)
+        data["cstrategy"] = data["strategy"].cumsum().apply(np.exp)
+        self.results = data 
+        # absolute performance of the strategy
+        perf = data["cstrategy"].iloc[-1]
+        # out-/underpeformance of the strategy
+        outperf = perf - data["creturns"].iloc[-1]
+        return round(perf, 6), round(outperf, 6)
+
